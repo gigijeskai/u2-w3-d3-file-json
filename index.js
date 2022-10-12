eb49a904d3a4693d51871974b937c6d533155ade
Luigi Indaco <luigindaco@gmail.com>
2022-10-12 14:51:54 +0200

    conversione del json

---
+++ index.js
@@ -0,0 +1,43 @@
+let datiDaJson = `{
+    "studenti" : [
+    {
+    "nome": "mario",
+    "cognome":"rossi",
+    "anni": 22,
+    "conocscenze": [
+        {"html": "base",
+        "css": "base",
+        "javascript":"beginner"
+    }
+    ],
+    "nazione": "italia" 
+},
+{
+    "nome": "marta",
+    "cognome":"pippo",
+    "anni": 52,
+    "conocscenze": [
+        {"html": "base",
+        "css": "nessuna",
+        "javascript":"avanzate"
+    }
+    ],
+    "nazione": "finlandia" 
+},
+{
+    "nome": "michele",
+    "cognome":"bianco",
+    "anni": 28,
+    "conocscenze": [
+        {"html": "nessuna",
+        "css": "avanzato",
+        "javascript":"base"
+    }
+    ],
+    "nazione": "spagna" 
+}
+    ]
+}`;
+
+let studenti = JSON.parse(datiDaJson);
+console.log(studenti);
