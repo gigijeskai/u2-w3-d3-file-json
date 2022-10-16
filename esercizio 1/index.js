bcd0a15386054b7a96f5023b6602b06133b6dd83
Luigi Indaco <luigindaco@gmail.com>
2022-10-16 21:00:30 +0200

    aggiunte

---
+++ esercizio 1/index.js
@@ -0,0 +1,53 @@
+let datiDaJson = `{
+    "studenti" : [
+    {
+    "nome": "mario",
+    "cognome":"rossi",
+    "anni": 22,
+    "conocscenze": [
+        {"html": "base",
+        "css": "base",
+        "javascript":"beginner"
+    }
+    ],
+    "nazione": "italia" 
+},
+{
+    "nome": "marta",
+    "cognome":"pippo",
+    "anni": 52,
+    "conocscenze": [
+        {"html": "base",
+        "css": "nessuna",
+        "javascript":"avanzate"
+    }
+    ],
+    "nazione": "finlandia" 
+},
+{
+    "nome": "michele",
+    "cognome":"bianco",
+    "anni": 28,
+    "conocscenze": [
+        {"html": "nessuna",
+        "css": "avanzato",
+        "javascript":"base"
+    }
+    ],
+    "nazione": "spagna" 
+}
+    ]
+}`;
+
+let studenti = JSON.parse(datiDaJson);
+let stringaStudenti = JSON.stringify(studenti);
+
+function mostraStudenti() {
+  localStorage.setItem("studenti", stringaStudenti);
+  let mostra = localStorage.getItem("studenti");
+  document.querySelector("div").innerHTML += mostra;
+}
+
+function cancellaStudenti() {
+  localStorage.removeItem("studenti");
+}
--- index.js
+++ index.js
@@ -1,53 +1,43 @@
-let datiDaJson = `{
-    "studenti" : [
-    {
-    "nome": "mario",
-    "cognome":"rossi",
-    "anni": 22,
-    "conocscenze": [
-        {"html": "base",
-        "css": "base",
-        "javascript":"beginner"
-    }
-    ],
-    "nazione": "italia" 
-},
-{
-    "nome": "marta",
-    "cognome":"pippo",
-    "anni": 52,
-    "conocscenze": [
-        {"html": "base",
-        "css": "nessuna",
-        "javascript":"avanzate"
-    }
-    ],
-    "nazione": "finlandia" 
-},
-{
-    "nome": "michele",
-    "cognome":"bianco",
-    "anni": 28,
-    "conocscenze": [
-        {"html": "nessuna",
-        "css": "avanzato",
-        "javascript":"base"
-    }
-    ],
-    "nazione": "spagna" 
-}
-    ]
-}`;
-
-let studenti = JSON.parse(datiDaJson);
-let stringaStudenti = JSON.stringify(studenti);
-
-function mostraStudenti() {
-  localStorage.setItem("studenti", stringaStudenti);
-  let mostra = localStorage.getItem("studenti");
-  document.querySelector("div").innerHTML += mostra;
+// qui salvo in local storage gli elementi di una lista
+let todoList = [];
+// array vuoto
+function addToList() {
+  // funzione per aggiungere alla lista
+  let inputValue = document.querySelector("#toDoInput").value;
+  // prendo il valore dell'input
+  todoList.push(inputValue);
+  // pusho nell'array il valore dell'input
+  let todoListString = JSON.stringify(todoList);
+  // converto in stringa l'array per poterlo salvare in locale
+  localStorage.setItem("todo-list", todoListString);
+  // una volta convertito in stringa lo salvo in locale e gli do una chiave e il valore
+  // della chiave
+  visualizeAllTodos();
+  // funzione per printare a schermo un todo
 }
 
-function cancellaStudenti() {
-  localStorage.removeItem("studenti");
+function visualizeAllTodos() {
+  // funzione per printare
+  let todoListDiv = document.querySelector("#todo-list");
+  todoListDiv.innerHTML = "";
+  // pulisco la div
+  for (let todo of todoList) {
+    todoListDiv.innerHTML += `<div>${todo}</div>`;
+  }
+  // ciclo per stampare a schermo in una div i todo
 }
+
+window.onload = function () {
+  // al caricamento della pagina si svolgerà la funzione, window prima di onload è opzionale
+  // perche' è sottointeso, stassa cosa per localstorage dove si può omettere il
+  // window.
+  let saveTodoSTring = localStorage.getItem("todo-list");
+  // prendo i dati in stringa salvati richiamando la chiave di prima e li metto in una variabile
+  if (saveTodoSTring !== null) {
+    // senza l'if andrebbe in errore perche' cercherebbe di convertire il nulla
+    todoList = JSON.parse(saveTodoSTring);
+    visualizeAllTodos();
+    // questo if se è vero mi converte l'array da stringa per poi poterlo printare a
+    // schermo
+  }
+};
